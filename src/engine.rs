425e6efb20919f6acc4dd649ece9a55b6ae583d1
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-02-19 21:54:33 +0900

    Add match_file function to main.rs

---
+++ src/engine.rs
@@ -0,0 +1,5 @@
+use std::error::Error;
+
+pub(crate) fn do_matching(expr: &str, line: &str) -> Result<bool, Box<dyn Error>> {
+    Ok(true)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
+mod engine;
+
+fn match_file(expr: &str, file_path: &str) -> Result<(), Box<dyn Error>> {
+    let f = File::open(file_path)?;
+    let reader = BufReader::new(f);
+    for line in reader.lines() {
+        let line = line?;
+        for (i, _) in line.char_indices() {
+            if engine::do_matching(expr, &line[i..])? {
+                println!("{line}");
+            }
+        }
+    }
+    Ok(())
+}
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = std::env::args().collect::<Vec<String>>();
+    println!("{}", args.len());
+    match args.len() {
+        3 => match_file(&args[1], &args[2]),
+        _ => {
+            eprintln!("usage: {} regex file", args[0]);
+            Err("invalid arguments".into())
+        }
+    }
 }
